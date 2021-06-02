211b45514866d49ddfe25dbf1e695fe44ad141bb
Samuel Cole <sam@samuelcole.name>
2021-06-02 03:24:05 +0000

    move ssl to nginx proxy

--- index.js
+++ index.js
@@ -6,30 +6,6 @@ const express = require("express");
 
 const app = express();
 
-// Certificate
-const privateKey = fs.readFileSync(
-  "/etc/letsencrypt/live/samuelcole.name/privkey.pem",
-  "utf8"
-);
-const certificate = fs.readFileSync(
-  "/etc/letsencrypt/live/samuelcole.name/cert.pem",
-  "utf8"
-);
-const ca = fs.readFileSync(
-  "/etc/letsencrypt/live/samuelcole.name/chain.pem",
-  "utf8"
-);
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
-
-app.use((req, res, next) => {
-  req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
-});
-
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
@@ -39,12 +15,7 @@ app.get("/", (req, res) => {
 
 // Starting both http & https servers
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(80, () => {
-  console.log("HTTP Server running on port 80");
-});
-
-httpsServer.listen(443, () => {
-  console.log("HTTPS Server running on port 443");
+httpServer.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
