0fbcdd296dd49e9e25fe6f5725e632b320a914ce
Samuel Cole <sam@samuelcole.name>
2021-06-04 02:47:41 +0000

    some logging

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // Dependencies
 const fs = require("fs");
 const http = require("http");
-const https = require("https");
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
+app.set("trust proxy", "loopback");
 app.use(express.static("public"));
+app.use(morgan("common"));
 
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// Starting both http & https servers
 const httpServer = http.createServer(app);
 
 httpServer.listen(5000, () => {
